a7d3307d193035cddd1f30a34e41d7a43bf19779
mdg_92 <mdg_92@yahoo.com.ar>
2015-11-24 04:22:06 -0300

    supuetsamente terminé con campus, sin g++ no se si compila. mañana veo de agregarlo

--- base/campus.h
+++ base/campus.h
@@ -1,7 +1,3 @@
-struct Pos{
-	Nat X;
-	Nat Y;
-};
 class Campus
 {
 	private:
@@ -10,17 +6,17 @@ class Campus
 		Arreglo<Arreglo<bool> > _grilla;
 	public:
 		Campus(Nat, Nat);
-		void agregarObs(Pos);
+		void agregarObs(posición);
 		Nat alto();
 		Nat ancho();
-		bool ocupada(Pos);
-		bool posValida(Pos);
-		bool esIngreso(Pos);
-		bool ingresoSup(Pos);
-		bool ingresoInf(Pos);
-		Nat distancia(Pos, Pos);
-		Conj<Pos> vecinos();
-		Pos aPosMasCercana(Pos,Conj<Pos>);
+		bool ocupada(posición);
+		bool posValida(posición);
+		bool esIngreso(posición);
+		bool ingresoSup(posición);
+		bool ingresoInf(posición);
+		Nat distancia(posición, posición);
+		Conj<posición> vecinos();
+		posición aPosMasCercana(posición, const Conj<posición>&);
 };
 
 Campus::Campus (Nat an, Nat al){
@@ -31,8 +27,8 @@ Campus::Campus (Nat an, Nat al){
 		this->_grilla.Definir(i,Arreglo<bool>(al));
 		}
 };
-void Campus::agregarObs(Pos obst){
-	this->_grilla[obst.X].Definir(obst.Y, true);
+void Campus::agregarObs(posición obst){
+	this->_grilla[obst.x].Definir(obst.y, true);
 };
 
 Nat alto(){
@@ -43,58 +39,103 @@ Nat ancho(){
 	return this->_ancho;
 };
 
-bool ocupada(Pos p){
-	return this->_grilla[p.X][p.Y];
+bool ocupada(posición p){
+	return this->_grilla[p.x][p.y];
 };
 
-bool posValida(Pos p){
-	return (p.X<this->_ancho)&&(p.Y<this->_alto);
+bool posValida(posición p){
+	return (p.x<this->_ancho)&&(p.y<this->_alto);
 };
 
-bool esIngreso(Pos p){
-	return (p.X==0)||(p.X==(this->_ancho -1));
+bool esIngreso(posición p){
+	return (p.y==0)||(p.y==(this->_alto -1));
 };
 
-bool ingresoSup(Pos p){
-	return p.X==0
+bool ingresoSup(posición p){
+	return p.y==0
 }
 
-bool ingresoInf(Pos p){
-	return p.X==this->_ancho -1;
+bool ingresoInf(posición p){
+	return p.y==this->_alto -1;
 }
 
-Nat distancia(Pos p1,p2){
+Nat distancia(posición p1,p2){
 	Nat resX;
 	Nat resY;
-	if(p1.X>p2.X){
-		resX=p1.X-p2.X;
+	if(p1.x>p2.x){
+		resX=p1.x-p2.x;
 	}else{
-		resX=p2.X-p1.X;
+		resX=p2.x-p1.x;
 	}
-	if(p1.Y>p2.Y){
-		resY=p1.Y-p2.Y;
+	if(p1.y>p2.y){
+		resY=p1.y-p2.y;
 	}else{
-		resY=p2.Y-p1.Y;
+		resY=p2.y-p1.y;
 	}
 	return resX+resY;
 }
-Conj<Pos> vecinos(Pos p){
-	  Conj<Pos> veci;
-	  Pos cand;
-	  cand.X=p.X-1;
-	  cand.Y=p.Y;
+Conj<posición> vecinos(posición p){
+	  Conj<posición> veci;
+	  posición cand;
+	  cand.x=p.x-1;
+	  cand.y=p.y;
 	  if(this->posValida(cand)) veci.agregarRapido(cand);
-	  cand.X+=2;
+	  cand.x+=2;
 	  if(this->posValida(cand)) veci.agregarRapido(cand);
-	  cand.X-=1;
-	  camd.Y-=1;
+	  cand.x-=1;
+	  camd.y-=1;
 	  if(this->posValida(cand)) veci.agregarRapido(cand);
-	  cand.Y+=2;
+	  cand.y+=2;
 	  if(this->posValida(cand)) veci.agregarRapido(cand);
 	  return veci;
 }
-Pos aPosMasCercana(Pos p, Conj<Pos> posi){
-	Pos cand;
-	Conj<Pos>::Iterador it=posi.CrearIt;
+posición aPosMasCercana(posición p, const Conj<posición>& posi){
+	Conj<posición>::Iterador it=posi.CrearIt();
+	posición cand=it.siguiente();
+	while (it.HaySiguiente()){
+		if (this->distancia(cand,p)>this->distancia(it.siguiente(),p){
+			cand=it.siguiente();
+			while (it.HayAnterior()) it.EliminarAnterior();
+			it.Avanzar();
+		}else{
+			if (this->distancia(cand,p)<this->distancia(it.siguiente(),p){
+				it.EliminarSiguiente();
+				cand=it.siguiente();
+			}else it.Avanzar();
+		}
+	}
+	it=posi.CrearIt();
+	cand=it.siguiente;
+	posicion ceero;
+	ceero.x=0;
+	ceero.y=0;
+	while (it.HaySiguiente()){
+		if (this->distancia(ceero,p)>this->distancia(it.siguiente(),ceero){
+			cand=it.siguiente();
+			while (it.HayAnterior()) it.EliminarAnterior();
+			it.Avanzar();
+		}else{
+			if (this->distancia(ceero,p)<this->distancia(it.siguiente(),ceero){
+				it.EliminarSiguiente();
+				cand=it.siguiente();
+			}else it.Avanzar();
+		}
+	}
+	it=posi.CrearIt();
+	cand=it.siguiente;
+	while (it.HaySiguiente()){
+		it=posi.CrearIt();
+		cand=it.siguiente;
+		if (cand.y<it.siguiente.y){
+			cand=it.siguiente();
+			while (it.HayAnterior()) it.EliminarAnterior();
+			it.Avanzar();
+		}else{
+			if (cand.y>it.siguiente.y){
+				it.EliminarSiguiente();
+				cand=it.siguiente();
+			}else it.Avanzar();
+		}
+	}
 	return cand;
 }
--- base/campuse_test.cpp
+++ base/campuse_test.cpp
@@ -2,12 +2,12 @@
 #include "aed2.h"
 #include "tipos.h"
 #include <iostream>
-  #include "aed2/Arreglo.h"
-  #include "aed2/Lista.h"
-  #include "aed2/Vector.h"
-  #include "aed2/Dicc.h"
-  #include "aed2/Conj.h"
-  #include "aed2/ConjAcotado.h"
+#include "aed2/Arreglo.h"
+#include "aed2/Lista.h"
+#include "aed2/Vector.h"
+#include "aed2/Dicc.h"
+#include "aed2/Conj.h"
+#include "aed2/ConjAcotado.h"
 
 int main (){
 	return 0;
